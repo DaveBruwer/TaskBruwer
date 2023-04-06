6c02b4599e698dec7a2e1da753a86421a3991351
Dave Bruwer <dave.bruwer@fphcare.co.nz>
2023-04-06 16:29:07 +1200

    updated Vite + Vue to Task Bruwer

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="../coffee-cup_icon-icons.com_69402.ico" alt="Task Bruwer">
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Task Bruwer" />
 </template>
 
 <style scoped>
